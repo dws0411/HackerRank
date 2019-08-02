6813105f535cb848db88e945753e3c161283d62a
戴维思 <962841737@qq.com>
2019-08-02 09:18:53 +0800

    Create playing_with_characters.c

---
+++ playing_with_characters.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MAX_LEN 128
+
+int main() 
+{
+    char c1;
+    char c2[MAX_LEN];
+    char c3[MAX_LEN];
+    
+    scanf("%c",&c1);
+    printf("%c\n",c1);
+
+    scanf("%s", c2);
+    printf("%s\n",c2);
+    scanf("\n");
+    scanf("%[^\n]",c3);
+    printf("%s",c3);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
