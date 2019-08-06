21b2a71e9cb8b14db13e29cdd1b62e063c546e62
戴维思 <962841737@qq.com>
2019-08-06 13:51:32 +0800

    Update Print_Tockens.c
    注释部分添加数组新的表示形式

--- Print_Tockens.c
+++ Print_Tockens.c
@@ -12,7 +12,7 @@ int main() {
     //Write your logic to print the tokens of the sentence here.
     int i;
     for(i =0; i<strlen(s);i++){
-        if(s[i] != 0x20){           //0x20是空格，即若字符串当前位置不为空格则打印，若为空格则换行
+        if(s[i] != 0x20){           //0x20是空格，即若字符串当前位置不为空格则打印，若为空格则换行;或表示为*(s+i)
             printf("%c",s[i]);
         }else{
             printf("\n");
